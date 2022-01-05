807edf8ff7453581861179a2b8e8521529b3ab53
Marc Wimmer <marc@itewimmer.de>
2022-01-05 19:58:34 +0100

    main: [main]

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(
     # If your package is a single module, use this instead of 'packages':
     #py_modules=['prlsnapshotter'],
     data_files=[
-        (metadata['name'], [
+        (metadata['name'].replace('-', '_'), [
             'data/bash',
         ])
     ],
